83ecfa0436322061b82e26105df1521fff0e7fff
Veershah26 <shah.veer18@gmail.com>
2021-04-17 13:42:40 +0530

    Added window position

--- main.c
+++ main.c
@@ -8,5 +8,5 @@ int main(int argc, char *argv[]) {
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Breakout Clone");
-    
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 }
