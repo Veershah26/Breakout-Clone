3c743ca6a0073c043d801e9248cb3f43768e702b
Veershah26 <shah.veer18@gmail.com>
2021-04-17 13:45:55 +0530

    Added window default size

--- main.c
+++ main.c
@@ -9,4 +9,5 @@ int main(int argc, char *argv[]) {
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Breakout Clone");
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
 }
