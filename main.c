c2c53714b86baa408396245f062ac71089d64418
Veershah26 <shah.veer18@gmail.com>
2021-04-17 13:40:57 +0530

    Added New Window & Window Title

--- main.c
+++ main.c
@@ -5,4 +5,8 @@ int main(int argc, char *argv[]) {
     GtkWidget *window;
 
     gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Breakout Clone");
+    
 }
